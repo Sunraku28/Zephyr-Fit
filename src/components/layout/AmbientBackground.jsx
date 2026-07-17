90f3ff64b99d3fdd561c8047a0b37629e951b78c
Anubhav Sharma <sharmaanubhav398@gmail.com>
2026-07-17 22:54:21 +0530

    ui updated

--- src/components/layout/AmbientBackground.jsx
+++ src/components/layout/AmbientBackground.jsx
@@ -1,17 +1,18 @@
 export default function AmbientBackground() {
-  const wavePath = "M0,60 Q300,120 600,60 T1200,60 L1200,120 L0,120 Z";
+  // Top-down wave path: fills the top, curvy at the bottom
+  const wavePath = "M0,0 L1200,0 L1200,100 C900,180 600,20 300,110 C150,150 50,90 0,90 Z";
   
   return (
     <div className="ambient" aria-hidden="true">
-      <div className="stars" />
+      <div className="plankton" />
       <div className="waves-container">
-        <svg className="wave w3" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <svg className="wave w3" viewBox="0 0 1200 200" preserveAspectRatio="none">
           <path d={wavePath} />
         </svg>
-        <svg className="wave w2" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <svg className="wave w2" viewBox="0 0 1200 200" preserveAspectRatio="none">
           <path d={wavePath} />
         </svg>
-        <svg className="wave w1" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <svg className="wave w1" viewBox="0 0 1200 200" preserveAspectRatio="none">
           <path d={wavePath} />
         </svg>
       </div>
